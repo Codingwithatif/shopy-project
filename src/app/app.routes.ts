1743d7a55d978faabeb1e50db4a708b08329bb72
Codingwithatif <atifnoor214@gmail.com>
2024-04-02 04:00:18 +0500

    navbar complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+
+
+
+
+];
